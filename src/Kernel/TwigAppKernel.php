85061fee4658998e2faa1c8cda93b7660b5222bb
Tanguy LEMARIE <tanguy.lemarie@skaleet.com>
2025-04-19 16:24:44 +0200

    EditorExtensionTest

---
+++ src/Kernel/TwigAppKernel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Pentiminax\UX\Editor\Kernel;
+
+use Pentiminax\UX\Editor\EditorBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\UX\StimulusBundle\StimulusBundle;
+
+class TwigAppKernel extends Kernel
+{
+    public function registerBundles(): iterable
+    {
+        return [new FrameworkBundle(), new TwigBundle(), new StimulusBundle(), new EditorBundle()];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(function (ContainerBuilder $container) {
+            $container->loadFromExtension('framework', ['secret' => '$ecret', 'test' => true, 'http_method_override' => false]);
+            $container->loadFromExtension('twig', ['default_path' => __DIR__.'/templates', 'strict_variables' => true, 'exception_controller' => null]);
+
+            $container->setAlias('test.editor.twig_extension', 'editor.twig_extension')->setPublic(true);
+        });
+    }
+}
---
+++ tests/Twig/EditorExtensionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pentiminax\UX\Editor\Tests\Twig;
+
+use Pentiminax\UX\Editor\Kernel\TwigAppKernel;
+use Pentiminax\UX\Editor\Model\Editor;
+use Pentiminax\UX\Editor\Twig\EditorExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class EditorExtensionTest extends KernelTestCase
+{
+    public function testRenderEditor(): void
+    {
+        $kernel = new TwigAppKernel('test', true);
+
+        $kernel->boot();
+
+        $container = $kernel->getContainer()->get('test.service_container');
+
+        $rendered = $container->get('test.editor.twig_extension')->renderEditor(new Editor());
+
+        $this->assertSame(
+            '<div id="editorjs" data-controller="pentiminax--ux-editor--editor" data-pentiminax--ux-editor--editor-view-value="[]"></div>',
+            $rendered
+        );
+    }
+}
