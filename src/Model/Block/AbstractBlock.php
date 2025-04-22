e9de7ab56c6b9361e5bb0b7633cd12d23aac4455
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-04-22 23:41:24 +0200

    Remove id generation

--- src/Model/Block/AbstractBlock.php
+++ src/Model/Block/AbstractBlock.php
@@ -13,7 +13,6 @@ abstract class AbstractBlock implements BlockInterface
 
     protected function __construct(BlockDataInterface $data)
     {
-        $this->id = self::generateBlockId();
         $this->data = $data;
     }
 
@@ -24,39 +23,11 @@ abstract class AbstractBlock implements BlockInterface
         return $this->data;
     }
 
-    public function getId(): string
-    {
-        return $this->id;
-    }
-
     public function jsonSerialize(): array
     {
         return [
-            'id' => $this->id,
             'type' => $this->getType()->value,
             'data' => $this->getData()
         ];
-    }
-
-    private static function generateBlockId(): string
-    {
-        $timestampMs = (string)round(microtime(true) * 1000);
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyz';
-
-        $timestampBase36 = '';
-        $timestamp = $timestampMs;
-        while ($timestamp !== '0') {
-            $remainder = bcmod($timestamp, '36');
-            $timestampBase36 = $chars[(int)$remainder] . $timestampBase36;
-            $timestamp = bcdiv($timestamp, '36');
-        }
-
-        $randomPart = '';
-        $bytes = random_bytes(5);
-        for ($i = 0; $i < 5; $i++) {
-            $randomPart .= $chars[ord($bytes[$i]) % 36];
-        }
-
-        return $timestampBase36 . $randomPart;
     }
 }
--- src/Model/Block/BlockInterface.php
+++ src/Model/Block/BlockInterface.php
@@ -8,8 +8,6 @@ interface BlockInterface extends \JsonSerializable
 {
     public function getData(): mixed;
 
-    public function getId(): string;
-
     public function getType(): BlockType;
 
 }
